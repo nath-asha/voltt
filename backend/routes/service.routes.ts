aec04f695784cd416fe94769d7c0b892d4bcee84
nath-asha <omthasha@gmail.com>
2023-10-31 08:48:37 +0530

    u

--- backend/routes/service.routes.ts
+++ backend/routes/service.routes.ts
@@ -1,53 +1,53 @@
 import { Router } from 'express';
 import {
-  getServices,
+  getAllServices,
   createService,
   updateService,
   deleteService,
-} from '../controllers/service.controller';
+} from '../controllers/servicesController';
+
+// import {
+//   getSocialfeed,
+//   createSocialfeed,
+//   updateSocialfeed,
+//   deleteSocialfeed,
+// } from '../controllers/social-feed.controller';
 
 import {
-  getSocialfeed,
-  createSocialfeed,
-  updateSocialfeed,
-  deleteSocialfeed,
-} from '../controllers/social-feed.controller';
-
-import {
-  getTestimonial,
+  getAllTestimonials,
   createTestimonial,
   updateTestimonial,
   deleteTestimonial,
-} from '../controllers/testimonial.controller';
+} from '../controllers/testimonialsController';
 
 import {
   getProject,
   createProject,
   updateProject,
   deleteProject,
-} from '../controllers/project.controller';
+} from '../controllers/projectsController';
 import authenticateJWT from './auth.middleware';
-import {createContact} from '../controllers/contact-us.controller' ;
+import {createContact} from '../controllers/contactController' ;
 import {upload} from './mutler';
 
 const router = Router();
 
-router.get('/services', getServices);
+router.get('/services', getAllServices);
 router.post('/services',(upload as any).single('file'), createService);
 router.put('/services/:id', updateService);
 router.delete('/services/:id', deleteService);
 
-router.get('/social-feed', getSocialfeed);
-router.post('/social-feed', createSocialfeed);
-router.put('/social-feed/:id', updateSocialfeed);
-router.delete('/social-feed/:id', deleteSocialfeed);
+// router.get('/social-feed', getSocialfeed);
+// router.post('/social-feed', createSocialfeed);
+// router.put('/social-feed/:id', updateSocialfeed);
+// router.delete('/social-feed/:id', deleteSocialfeed);
 
 router.get('/project', getProject);
 router.post('/project', authenticateJWT, createProject);
 router.put('/project/:id',authenticateJWT, updateProject);
 router.delete('/project/:id', deleteProject);
 
-router.get('/testimonial', getTestimonial);
+router.get('/testimonial', getAllTestimonials);
 router.post('/testimonial', createTestimonial);
 router.put('/testimonial/:id', updateTestimonial);
 router.delete('/testimonial/:id', deleteTestimonial);
